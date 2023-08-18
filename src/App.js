4701f72cc54d8fc7cf8ebd8e1e7fe4a5667cc328
Samar Pratap Singh <57188508+samar-1601@users.noreply.github.com>
2023-08-18 13:31:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <div className="grey-area"></div>
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
